1db36353fbacae5287ba225904680b2cea81fc93
thisismyrobot <rwallhead@gmail.com>
2009-09-02 18:47:00 -0700

    Added GNU GPL 3 licence

--- google_calculator.py
+++ google_calculator.py
@@ -3,13 +3,30 @@
 
     Designed for python 2.6
 
-    Tested on Windows XP (so far)
+    Tested on Windows XP (so far) - should work on *nix systems
 
     Usage: "main.py COM5"
 
     Requires: 
         pyserial - http://pyserial.sourceforge.net/
         google api - http://github.com/thisismyrobot/Google-API/
+
+    Licensing
+    ---------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 """
 
 import serial
